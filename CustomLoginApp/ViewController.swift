9647cb0a70a3833414935c14a4c7c6a02dbecc91
Vladchere <vladchere.ok@mail.ru>
2021-04-01 22:48:35 +0700

    Initial Commit

---
+++ CustomLoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomLoginApp
+//
+//  Created by Vladislav on 01.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
